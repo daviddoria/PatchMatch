8c0a7d9d9ce37592ba37ed8f82b8876554e58034
David Doria <david.l.doria.civ@mail.mil>
2015-05-18 16:03:51 -0400

    Remove unnecessary files.

--- Examples/GroundTruthNNField.cpp
+++ Examples/GroundTruthNNField.cpp
@@ -11,9 +11,9 @@
 int main(int argc, char*argv[])
 {
   // Verify arguments
-  if(argc < 5)
+  if(argc < 4)
   {
-    std::cerr << "Required arguments: image mask patchRadius output" << std::endl;
+    std::cerr << "Required arguments: image patchRadius output" << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -24,15 +24,13 @@ int main(int argc, char*argv[])
     ss << argv[i] << " ";
   }
   std::string imageFilename;
-  std::string maskFilename;
   unsigned int patchRadius;
   std::string outputFilename;
 
-  ss >> imageFilename >> maskFilename >> patchRadius >> outputFilename;
+  ss >> imageFilename >> patchRadius >> outputFilename;
 
   // Output arguments
   std::cout << "imageFilename: " << imageFilename << std::endl;
-  std::cout << "maskFilename: " << maskFilename << std::endl;
   std::cout << "patchRadius: " << patchRadius << std::endl;
   std::cout << "outputFilename: " << outputFilename << std::endl;
 
@@ -45,15 +43,6 @@ int main(int argc, char*argv[])
 
   ImageType* image = imageReader->GetOutput();
 
-  // The target mask should be valid in the region to fill
-  Mask::Pointer targetMask = Mask::New();
-  targetMask->Read(maskFilename);
-  targetMask->InvertData();
-
-  // The source mask should be valid in the region from which to take source patches
-  Mask::Pointer sourceMask = Mask::New();
-  sourceMask->Read(maskFilename);
-
   SSD<ImageType>* patchDistanceFunctor = new SSD<ImageType>;
   patchDistanceFunctor->SetImage(imageReader->GetOutput());
   
@@ -61,42 +50,37 @@ int main(int argc, char*argv[])
   SelfPatchCompareType selfPatchCompare;
   selfPatchCompare.SetPatchDistanceFunctor(patchDistanceFunctor);
   selfPatchCompare.SetImage(image);
-  selfPatchCompare.SetMask(sourceMask);
+  selfPatchCompare.CreateFullyValidMask();
 
-  itk::ImageRegion<2> targetMaskBoundingBox = MaskOperations::ComputeValidBoundingBox(targetMask);
-  
-  itk::ImageRegionIteratorWithIndex<ImageType> imageIterator(image, targetMaskBoundingBox);
+  itk::ImageRegionIteratorWithIndex<ImageType> imageIterator(image, image->GetLargestPossibleRegion());
 
   PatchMatchHelpers::CoordinateImageType::Pointer nnField =
        PatchMatchHelpers::CoordinateImageType::New();
   nnField->SetRegions(image->GetLargestPossibleRegion());
   nnField->Allocate();
 
-  unsigned int numberOfTargetPixels = targetMask->CountValidPixels();
   unsigned int numberOfCompletedPixels = 0;
   while(!imageIterator.IsAtEnd())
   {
-    if(targetMask->IsValid(imageIterator.GetIndex()))
-    {
-      std::cout << "Computing NN for " << imageIterator.GetIndex() << std::endl;
-      itk::ImageRegion<2> targetRegion =
-           ITKHelpers::GetRegionInRadiusAroundPixel(imageIterator.GetIndex(), patchRadius);
-      selfPatchCompare.SetTargetRegion(targetRegion);
-      selfPatchCompare.ComputePatchScores();
-      std::vector<SelfPatchCompareType::PatchDataType> patchData = selfPatchCompare.GetPatchData();
-      std::partial_sort(patchData.begin(), patchData.begin() + 2, patchData.end(),
-                        Helpers::SortBySecondAccending<SelfPatchCompareType::PatchDataType>);
+    std::cout << "Computing NN for " << imageIterator.GetIndex() << std::endl;
+    itk::ImageRegion<2> targetRegion =
+       ITKHelpers::GetRegionInRadiusAroundPixel(imageIterator.GetIndex(), patchRadius);
+    selfPatchCompare.SetTargetRegion(targetRegion);
+    selfPatchCompare.ComputePatchScores();
+    std::vector<SelfPatchCompareType::PatchDataType> patchData = selfPatchCompare.GetPatchData();
+    std::partial_sort(patchData.begin(), patchData.begin() + 2, patchData.end(),
+                    Helpers::SortBySecondAccending<SelfPatchCompareType::PatchDataType>);
 
-      itk::Index<2> bestMatchCenter = ITKHelpers::GetRegionCenter(patchData[0].first);
-      PatchMatchHelpers::CoordinateImageType::PixelType nnFieldPixel;
-      nnFieldPixel[0] = bestMatchCenter[0];
-      nnFieldPixel[1] = bestMatchCenter[1];
-      nnFieldPixel[2] = patchData[0].second;
-      std::cout << "NN is " << nnFieldPixel << std::endl;
-      nnField->SetPixel(imageIterator.GetIndex(), nnFieldPixel);
-    }
+    itk::Index<2> bestMatchCenter = ITKHelpers::GetRegionCenter(patchData[0].first);
+    PatchMatchHelpers::CoordinateImageType::PixelType nnFieldPixel;
+    nnFieldPixel[0] = bestMatchCenter[0];
+    nnFieldPixel[1] = bestMatchCenter[1];
+    nnFieldPixel[2] = patchData[0].second;
+    std::cout << "NN is " << nnFieldPixel << std::endl;
+    nnField->SetPixel(imageIterator.GetIndex(), nnFieldPixel);
+
     numberOfCompletedPixels++;
-    std::cout << "Completed " << numberOfCompletedPixels << " of " << numberOfTargetPixels << std::endl;
+
     ++imageIterator;
   }
 
